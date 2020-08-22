bb3ac22a5932bca3c23243f7ef26b7e5e6de4d56
lytves <22356770+lytves@users.noreply.github.com>
2020-08-22 18:29:54 +0200

    Blocks refactoring with implementation of Open Closed Principle SOLID

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import {model} from "./model";
 import './styles/main.css'
-import {templates} from "./templates";
 
 const site = document.getElementById("site");
 
 model.forEach((block) => {
-    let generate = templates[block.type]
     console.warn(block);
-    if (generate) {
-        const html = generate(block)
-        site.insertAdjacentHTML('beforeend', html)
-    } else {
-        console.warn('faileddd');
-    }
+    site.insertAdjacentHTML('beforeend', block.toHTML())
 })
--- src/model.js
+++ src/model.js
@@ -1,24 +1,24 @@
 import image from './assets/image.jpg'
-import {Block} from './classes/blocks'
+import {ImageBlock, TextBlock, TextColumnsBlock, TitleBlock} from './classes/blocks'
 
 export const model = [
-    new Block('title', 'CMS on pure JavaScript!', {
+    new TitleBlock('CMS on pure JavaScript!', {
         tag: 'h2',
         styles: 'background: #eb425e; color: #fff; text-align: center; padding: 0.5rem'
     }),
-    new Block('textColums', [
+    new TextColumnsBlock( [
         'Application on pure JavaScript without 3rd party frameworks',
         'Using SOLID principles',
         'Based on WebPack'
     ], {
         styles: 'padding: 1rem;'
     }),
-    new Block('text',
+    new TextBlock(
         '\'Learning JavaScript is fun and useful, be creative and modern frontend developer!\'',
         {
             styles: 'background: #f5f5f5; color: #2f2f2f; padding: 1rem;'
         }),
-    new Block('image', image,
+    new ImageBlock( image,
         {
             styles: 'padding: 1rem; display: flex; justify-content: center;',
             imageStyles: 'height: 200px;',
