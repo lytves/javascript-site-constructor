2d930b43658dada066d1131e1cf7406e0b0dfeaf
lytves <22356770+lytves@users.noreply.github.com>
2020-08-23 19:24:09 +0200

    refactoring method css to improve model Object

--- src/model.js
+++ src/model.js
@@ -1,27 +1,43 @@
 import image from './assets/image.jpg'
 import {ImageBlock, TextBlock, TextColumnsBlock, TitleBlock} from './classes/blocks'
+import {css} from "./utils";
 
 export const model = [
     new TitleBlock('CMS on pure JavaScript!', {
         tag: 'h2',
-        styles: 'background: #eb425e; color: #fff; text-align: center; padding: 0.5rem'
+        styles: css({
+            background: '#eb425e',
+            color: '#fff',
+            'text-align': 'center',
+            padding: '0.5rem'
+        })
     }),
     new TextColumnsBlock( [
         'Application on pure JavaScript without 3rd party frameworks',
         'Using SOLID principles',
         'Based on WebPack'
     ], {
-        styles: 'padding: 1rem;'
+        styles: css({
+            padding: '2rem'
+        })
     }),
     new TextBlock(
         '\'Learning JavaScript is fun and useful, be creative and modern frontend developer!\'',
         {
-            styles: 'background: #f5f5f5; color: #2f2f2f; padding: 1rem;'
+            styles: css({
+                background: '#c7c5b9',
+                color: '#2f2f2f',
+                padding: '1rem'
+            })
         }),
     new ImageBlock( image,
         {
-            styles: 'padding: 1rem; display: flex; justify-content: center;',
             imageStyles: 'height: 200px;',
-            alt: 'CMS on pure JavaScript!'
+            alt: 'CMS on pure JavaScript!',
+            styles: css({
+                padding: '1rem',
+                display: 'flex',
+                'justify-content': 'center'
+            })
         })
 ]
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,8 @@ export function colSm(content) {
     return `<div class="col-sm">${content}</div>`
 
 }
+
+export function css(styles = {}) {
+    const toString = (key) => `${key}: ${styles[key]}`
+    return Object.keys(styles).map(toString).join(';')
+}
